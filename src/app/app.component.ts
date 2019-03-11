ab63f5a0413aff8b5e3d2539edac0ea2dd3fb64f
LilyThomas <anneliesthomas94@gmail.com>
2019-03-11 16:35:32 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {AuthService} from './services/auth.service';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
-export class AppComponent {
-  title = 'spa';
+export class AppComponent implements OnInit{
+  title = 'Lily & Simon\'s SPA';
+
+
+  constructor(private auth: AuthService, private http: HttpClient){
+    auth.handleAuthentication();
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      this.auth.renewTokens();
+    }
+  }
+
+
 }
