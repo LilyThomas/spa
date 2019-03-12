724a510092f26136e14148323c2d7dac2bdeb455
LilyThomas <anneliesthomas94@gmail.com>
2019-03-12 13:51:08 +0100

    hopefull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {AuthService} from './services/auth.service';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {JwksValidationHandler, OAuthService} from "angular-oauth2-oidc";
+import { authConfig } from "./services/auth.service";
+import {logger} from "codelyzer/util/logger";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,18 +12,60 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   styleUrls: ['./app.component.css'],
   providers: [AuthService]
 })
-export class AppComponent implements OnInit{
-  title = 'Lily & Simon\'s SPA';
+export class AppComponent{
+  title = 'Lily\'s & Simon\'s SPA';
 
+  private _idToken: string;
+  private _accessToken: string;
+  private _expiresAt: number;
 
-  constructor(private auth: AuthService, private http: HttpClient){
-    auth.handleAuthentication();
+  constructor(private oauthService: OAuthService, private router: Router){
+    this.configureWithNewConfigApi();
+      this._idToken = '';
+      this._accessToken = '';
+      this._expiresAt = 0;
   }
 
-  ngOnInit() {
-    if (localStorage.getItem('isLoggedIn') === 'true') {
-      this.auth.renewTokens();
-    }
+  get accessToken(): string {
+    return this._accessToken;
+  }
+
+  get idToken(): string {
+    return this._idToken;
+  }
+
+  private configureWithNewConfigApi() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    // Call this.oauthService.tryLogin() if discovery document is not used.
+    // All configurations must be set manually.
+  }
+
+  login() {
+    logger.debug("hello");
+    this.oauthService.loadDiscoveryDocumentAndLogin().then(data => {
+      console.log("Logged in");
+      localStorage.setItem('isLoggedIn', 'true');
+      // const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+      console.log(this.oauthService.getAccessTokenExpiration());
+      // this._accessToken = this.oauthService.getAccessToken();
+      // this._idToken = this.oauthService.getIdToken();
+      // this._expiresAt = this.oauthService.getAccessTokenExpiration();
+      this.router.navigate(['']);
+      this.oauthService.initImplicitFlow();
+    }).catch(err => {
+      console.log("Unable to login");
+    });
+    logger.debug(this.oauthService.getIdToken());
+  }
+
+  logout() {
+    this.oauthService.logOut();
+    console.log("Logged out");
+  }
+
+  isAuthenticated() {
+    return new Date().getTime() < this.oauthService.getAccessTokenExpiration();
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AuthService} from './services/auth.service';
 import { HttpClientModule} from "@angular/common/http";
+import {Routes} from "@angular/router";
+import {OAuthModule } from "angular-oauth2-oidc-codeflow-pkce";
+import { OAuthService, UrlHelperService } from "angular-oauth2-oidc";
+
+export const routes: Routes = [
+  { path: '', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +20,10 @@ import { HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    OAuthModule.forRoot(),
   ],
-  providers: [],
+  providers: [OAuthService, UrlHelperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
